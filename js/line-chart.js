33f5cb68bd180a08c3d434a423618d29f382cd1e
Miyayx <yangxue00.yxmn@gmail.com>
2013-12-10 11:45:20 +0800

    Add line-chart in index.html, which show the time graph of all tweets

--- js/line-chart.js
+++ js/line-chart.js
@@ -1,10 +1,26 @@
+Date.prototype.Format = function (fmt) { //author: meizz 
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
 var lineChart = function(chartid, json){
 
   var margin = {top: 20, right: 20, bottom: 30, left: 50},
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
   
-  var parseDate = d3.time.format("%d-%b-%y").parse;
+  var parseDate = d3.time.format("%y.%m.%d").parse;
   
   var x = d3.time.scale()
       .range([0, width]);
@@ -24,17 +40,39 @@ var lineChart = function(chartid, json){
       .x(function(d) { return x(d.date); })
       .y(function(d) { return y(d.close); });
   
-  var svg = d3.select("body").append("svg")
+  var svg = d3.select(chartid).append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
-    .append("g")
+      .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
   
-    json.forEach(function(d) {
-      //d.date = parseDate(d.date);
-      d.date = parseDate(d.date);
-      d.close = +d.close;
-  
+    day_count = Object();
+    json.forEach(function(d){
+      var time = new Date(Math.floor(Number(d.time)) * 1000).Format("yy.MM.dd");
+      //day = time.getFullYear()+"."+time.getMonth()+"."+time.getDay();
+      day = time;
+      if(day_count.hasOwnProperty(day))
+        day_count[day] += 1;
+      else
+        day_count[day] = 1;
+        
+      });
+
+      data = Array();
+      for(var date in day_count){
+         var d = Object();
+      d.date = parseDate(date);
+      d.close = +day_count[date];
+      data.push(d);
+      }
+
+      function sortDate(a,b)
+      {
+         return a.date - b.date;
+      }
+
+      data = data.sort(sortDate);
+
     x.domain(d3.extent(data, function(d) { return d.date; }));
     y.domain(d3.extent(data, function(d) { return d.close; }));
   
@@ -46,17 +84,16 @@ var lineChart = function(chartid, json){
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis)
-      append("text")
+        .append("text")
         .attr("transform", "rotate(-90)")
         .attr("y", 6)
         .attr("dy", ".71em")
         .style("text-anchor", "end")
-        .text("Price ($)");
+        .text("");
   
     svg.append("path")
         .datum(data)
         .attr("class", "line")
         .attr("d", line);
-  });
   
 }
